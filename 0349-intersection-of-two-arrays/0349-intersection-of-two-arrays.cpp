bf72b2cbf9b70aa97d1d8ac7f1d6a23b61bd79fa
TusharBorase995 <tborase995@gmail.com>
2026-09-19 22:04:33 +0530

    Time: 2 ms (43.79%), Space: 14.2 MB (66.56%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -6,15 +6,12 @@ public:
         for(int x: nums1){
             mp[x]=1;
         }
-        int n= nums1.size();
         int m= nums2.size();
 
         for(int i=0;i<m;i++){
             if(mp.find(nums2[i])!=mp.end()){
-                if(mp[nums2[i]]!=2){
-                    ans.push_back(nums2[i]);
-                    mp[nums2[i]] =2;
-                }
+                ans.push_back(nums2[i]);
+                mp.erase(nums2[i]);
             }
         }
         return ans;
